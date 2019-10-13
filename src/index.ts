4c91fff2ae47c45cff68c95bf73b4e3037defaf5
Brian Le <brianle1301@gmail.com>
2019-10-13 16:28:01 +0100

    [workspace] Prep for another publish

--- src/index.ts
+++ src/index.ts
@@ -1,80 +1,35 @@
-import Client, { ClientOptionsArg, ClientOptions } from './Client';
-import Addon, { AddonOptions } from './Addon';
-import Dispatcher, { CommandComponents } from './Dispatcher';
-import Store, { FolderNames, StoreOptions, StoreOptionsArg, Resource, ResourceInfo } from './Store';
-import Command, {
-  CommandOptionsArg,
-  CommandOptions,
-  LimitOptions,
-  LimitScopes,
-  SubcommandsOptions,
-} from './Command';
-import Task, { TaskOptions } from './Task';
-import Permissions, { PermissionCheck } from './Permissions';
+import Client from './Client';
+import Addon from './Addon';
+import Dispatcher from './Dispatcher';
+import Store from './Store';
+import Command from './Command';
+import Task from './Task';
+import Permissions from './Permissions';
 import NebulaError from './NebulaError';
-import Validator, {
-  Primitives,
-  Schema,
-  ValueStoreEntry,
-  ValueStore,
-  ValidationRule,
-  ValidationResults,
-  ValidationFlags,
-  ValidatorOptions,
-} from './Validator';
+import Validator from './Validator';
 import BaseValidator from './Validator/BaseValidator';
 import BooleanValidator from './Validator/BooleanValidator';
 import NumberValidator from './Validator/NumberValidator';
 import StringValidator from './Validator/StringValidator';
 import ValidationError from './Validator/ValidationError';
-import Debugger, { LogTypes } from './Debugger';
+import Debugger from './Debugger';
 import Util from './Util';
-import { Constructor, MakeRequired, MakeOptsOptional, MakeOptsRequired } from './types';
 
 export {
   Client,
-  ClientOptionsArg,
-  ClientOptions,
   Addon,
-  AddonOptions,
   Store,
-  StoreOptions,
-  StoreOptionsArg,
-  Resource,
-  ResourceInfo,
-  FolderNames,
   Dispatcher,
-  CommandComponents,
   Command,
-  CommandOptionsArg,
-  CommandOptions,
-  LimitOptions,
-  LimitScopes,
-  SubcommandsOptions,
   Task,
-  TaskOptions,
   Permissions,
-  PermissionCheck,
   NebulaError,
   Validator,
-  Primitives,
-  Schema,
-  ValueStoreEntry,
-  ValueStore,
-  ValidationRule,
-  ValidationResults,
-  ValidationFlags,
-  ValidatorOptions,
   BaseValidator,
   BooleanValidator,
   NumberValidator,
   StringValidator,
   ValidationError,
   Debugger,
-  LogTypes,
   Util,
-  Constructor,
-  MakeRequired,
-  MakeOptsOptional,
-  MakeOptsRequired,
 };
