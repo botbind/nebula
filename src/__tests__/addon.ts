9d3ffd7a5775cd5610664c02a8fe9e2d718c98da
Brian Le <brianle1301@gmail.com>
2019-10-17 18:24:04 +0100

    [core] Tests using default folder names

--- src/__tests__/addon.ts
+++ src/__tests__/addon.ts
@@ -1,9 +1,18 @@
-import { Client, Addon } from '..';
+import { Client, Addon, Store } from '..';
+
+class TestStore extends Store {
+  constructor(addon: Addon) {
+    super(addon, {
+      baseDir: __dirname,
+    });
+  }
+}
 
 export default class TestAddon extends Addon {
   constructor(client: Client) {
     super(client, {
       name: 'test-addon',
+      store: TestStore,
     });
   }
 }
