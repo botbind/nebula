0ba4843904334ce3d6d2f84e99d6d2b4bca2627d
Brian Le <brianle1301@gmail.com>
2019-12-19 09:11:43 +0700

    [core] Refactored Addon and move Resource

--- src/core/Addon.ts
+++ src/core/Addon.ts
@@ -1,135 +1,111 @@
-import Discord from 'discord.js';
+import L from '@botbind/lyra';
 import Client from './Client';
 import Store from './Store';
 import Dispatcher from './Dispatcher';
 import Permissions from './Permissions';
-import Validator from './Validator';
-import Debugger from './Debugger';
-import Util from './Util';
 import Event from './Event';
-import NebulaError from './NebulaError';
-import * as Constants from './constants';
-import { Constructor } from './types';
+import Debugger from '../utils/Debugger';
+import { Constructor } from '../utils/types';
 
 /**
- * The options for the addon
+ * The options for the addon.
  */
 export interface AddonOptions {
   /**
-   * The name of the addon
+   * The name of the addon.
    */
   name: string;
 
   /**
-   * The customised implementation of Validator
-   */
-  validator?: Constructor<Validator>;
-
-  /**
-   * The customised implementation of Store
+   * The customised implementation of `Store`.
    */
   store?: Constructor<Store>;
 
   /**
-   * The customised implementation of dispatcher
+   * The customised implementation of `Dispatcher`.
    */
   dispatcher?: Constructor<Dispatcher>;
 
   /**
-   * The customised implementation of Permissions
+   * The customised implementation of `Permissions`.
    */
   permissions?: Constructor<Permissions>;
 }
 
 export default class Addon {
   /**
-   * The client of the addon
+   * The client of the addon.
    */
   public client: Client;
 
   /**
-   * The store of the addon
+   * The store of the addon.
    */
   public store: Store;
 
   /**
-   * The validator of the addon
-   */
-  public validator: Validator;
-
-  /**
-   * The permissions of the addon
+   * The permissions of the addon.
    */
   public permissions: Permissions;
 
   /**
-   * The dispatcher of the addon
+   * The dispatcher of the addon.
    */
   public dispatcher: Dispatcher;
 
   /**
-   * The name of the addon
+   * The name of the addon.
    */
   public name: string;
 
   /**
-   * The entry point of all Nebula resources
-   * @param client The client of the addon
-   * @param options The options of the addon
+   * The entry point of all Nebula resources.
+   * @param client The client of the addon.
+   * @param options The options of the addon.
    */
   constructor(client: Client, options: AddonOptions) {
-    if (Constants.IS_DEV && !Util.isObject(options))
-      throw new NebulaError(Constants.ERROR_MESSAGES['addon.options']);
+    const result = L.object({
+      name: L.string().required(),
+      store: L.function<Constructor<Store>>()
+        .inherit(Store)
+        .default(Store, { literal: true }),
+      permissions: L.function<Constructor<Permissions>>()
+        .inherit(Permissions)
+        .default(Permissions, { literal: true }),
+      dispatcher: L.function<Constructor<Dispatcher>>()
+        .inherit(Dispatcher)
+        .default(Dispatcher, { literal: true }),
+    })
+      .label('addonOptions')
+      .validate(options);
+
+    if (result.errors !== null) throw result.errors[0];
 
     const {
       name,
-      store: CustomisedStore,
-      dispatcher: CustomisedDispatcher,
-      validator: CustomisedValidator,
-      permissions: CustomisedPermissions,
-    } = options;
-
-    if (Constants.IS_DEV) {
-      if (typeof name === 'string')
-        throw new NebulaError(Constants.ERROR_MESSAGES['addon.options.name']);
-
-      if (CustomisedStore != null && !(CustomisedStore.prototype instanceof Store))
-        throw new NebulaError(Constants.ERROR_MESSAGES['addon.options.store']);
-
-      if (CustomisedDispatcher != null && !(CustomisedDispatcher.prototype instanceof Dispatcher))
-        throw new NebulaError(Constants.ERROR_MESSAGES['addon.options.dispatcher']);
-
-      if (CustomisedValidator != null && !(CustomisedValidator.prototype instanceof Validator))
-        throw new NebulaError(Constants.ERROR_MESSAGES['addon.options.validator']);
-
-      if (
-        CustomisedPermissions != null &&
-        !(CustomisedPermissions.prototype instanceof Permissions)
-      )
-        throw new NebulaError(Constants.ERROR_MESSAGES['addon.options.permissions']);
-    }
+      store: CustomizedStore,
+      dispatcher: CustomizedDispatcher,
+      permissions: CustomizedPermissions,
+    } = result.value;
 
     this.client = client;
     this.name = name;
-    this.store = CustomisedStore != null ? new CustomisedStore(this) : new Store(this);
-    this.dispatcher =
-      CustomisedDispatcher != null ? new CustomisedDispatcher(this) : new Dispatcher(this);
-    this.validator = CustomisedValidator != null ? new CustomisedValidator(this) : new Validator();
-    this.permissions =
-      CustomisedPermissions != null ? new CustomisedPermissions(this) : new Permissions(this);
+    this.store = new CustomizedStore(this);
+    this.dispatcher = new CustomizedDispatcher(this);
+    this.permissions = new CustomizedPermissions(this);
 
     // Has to be done after the addon has done loading other classes
     this.store.load();
 
-    // Group event dispatchers
-    const onceEvents = new Discord.Collection<string, Event[]>();
-    const onEvents = new Discord.Collection<string, Event[]>();
+    // Group event dispatchers.
+    const onceEvents = new Map<string, Event[]>();
+    const onEvents = new Map<string, Event[]>();
 
     this.store.events.forEach(event => {
       const eventCollection = event.once ? onceEvents : onEvents;
       const fetchedEvent = eventCollection.get(event.name);
 
-      if (fetchedEvent == null) eventCollection.set(event.name, [event]);
+      if (fetchedEvent === undefined) eventCollection.set(event.name, [event]);
       else fetchedEvent.push(event);
     });
 
@@ -152,7 +128,7 @@ export default class Addon {
       this.dispatcher.invokeLifecycles(message);
     });
 
-    if (this.client.shouldEditCommandResponses) {
+    if (this.client.shouldEditCommandResponses)
       this.client
         .on('messageUpdate', (oldMessage, newMessage) => {
           if (oldMessage.content === newMessage.content) return;
@@ -162,7 +138,6 @@ export default class Addon {
         .on('messageDelete', message => {
           this.dispatcher.deleteResponses(message);
         });
-    }
 
     Debugger.success(`${this.constructor.name} injected`);
   }
