d983a7c9637c4d6d886da45b1b4088a81fa851a7
Brian Le <brianle1301@gmail.com>
2020-03-14 23:35:56 +0000

    [tests] Fix tests

--- tests/index.js
+++ tests/index.js
@@ -1,3 +1,4 @@
+require('dotenv/config');
 const Nebula = require('../src');
 const Repeater = require('./addons/repeater');
 
@@ -11,7 +12,7 @@ client.on('nebulaReady', () => {
   client.logger.success('Client ready!');
 });
 
-client.login('Njg1OTMxNjgxOTk5MjI0ODU2.XmqfOw.GtWbPFWe29vUCZV4cajBTF2mmwg');
+client.login(process.env.NODE_ENV.DISCORD_TOKEN);
 
 process.on('unhandledRejection', err => {
   console.log(err);
